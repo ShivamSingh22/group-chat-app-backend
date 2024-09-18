c0131037456373eb299348ba0d51b3530d163651
ShivamSingh22 <singh.shivam2238@gmail.com>
2024-09-18 22:27:07 +0530

    signup api

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const cors=require('cors');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const userRoutes = require('./routes/userRoute');
+
+const sequelize = require('./util/database');
+
+const User= require('./models/userModel');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json({ extended: false }));
+
+app.use('/user',userRoutes);
+
+sequelize
+.sync()
+.then(result => {
+    app.listen(process.env.PORT || 3000);
+    console.log(`Listening on ${process.env.PORT}`);
+    
+})
+.catch(err => {
+    console.log(err);
+})
---
+++ controller/userController.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const User = require("../models/userModel");
+const bcrypt = require('bcrypt');
+
+exports.postSignup = async (req, res, next) => {
+    const { username } = req.body;
+    const { email } = req.body;
+    const { password } = req.body;
+    const { contact } = req.body;
+  
+    try {
+      const existingUser = await User.findOne({ where: { email } });
+  
+      if (existingUser) {
+        return res.status(403).json({ message: "ERROR: USER ALREADY EXISTS" });
+      } else {
+          bcrypt.hash(password, saltRounds, async function(err, hash) {
+              if(err){
+                  return res.status(400).json({hash:"Couldn't hash error"});
+              }else{
+                  await User.create({
+                      username: username,
+                      email: email,
+                      password: hash,
+                      contact: contact
+                    });
+              }
+          });
+        
+        res.status(201).json({ message: "Signup Completed!!" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
